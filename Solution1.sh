36711d0fd8f290bc3a0a93f01067aff56a45794d
omkar <omkardudhewar121@gmail.com>
2022-07-20 09:22:03 +0530

    [omkar]

---
+++ Solution1.sh
@@ -0,0 +1,11 @@
+Input: n = 5
+Output: 1 
+Factorial of 5 is 120 which has one trailing 0.
+
+Input: n =  10
+Output: 2
+Factorial of 20 is 3628800 which has two trailing zeroes.
+
+Input: n = 4
+Output: 0
+Factorial of 4 is 24 which zero trailling zeros.
